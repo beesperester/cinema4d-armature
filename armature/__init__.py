1f0821544ba7b259bbeb913154dc113106a10d51
Bernhard Esperester <bernhard@esperester.de>
2021-03-22 16:01:53 +0100

    promotes modules

---
+++ armature/__init__.py
@@ -0,0 +1,9 @@
+from armature.modules.armature import (
+    Armature,
+    ArmatureAdapter,
+    ArmatureModule,
+    ArmatureAdapters,
+    ArmatureModules,
+)
+
+from armature.modules.hierarchy import Hierarchy
